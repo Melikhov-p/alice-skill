6d070dd8b754a6e64a29aebdd76b638e22368576
Melikhov-p <melikhov.pavel@gmail.com>
2024-10-18 16:33:11 +0300

    feat(skill): reformat project structure, add base hook for POST

---
+++ cmd/skill/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(fmt.Sprintf(`Unhandled error %v`, err.Error()))
+	}
+}
+
+func run() error {
+	return http.ListenAndServe(`localhost:8080`, http.HandlerFunc(WebHook))
+}
+
+func WebHook(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		fmt.Fprintf(writer, `Method not allowed %d`, http.StatusMethodNotAllowed)
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+
+		return
+	}
+
+	writer.Header().Set(`Content-Type`, `application/json`)
+
+	_, _ = writer.Write([]byte(`{
+        "response": {
+          "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+      }`))
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module alice-skill
+module github.com/Melikhov-p/alice-skill
+
+go 1.23
